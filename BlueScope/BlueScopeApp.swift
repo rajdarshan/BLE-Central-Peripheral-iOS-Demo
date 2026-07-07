78277c5c8a0551c7d3c0879f0d4ee6f45ba93b7f
Darshan raju <rajdarshan1@gmail.com>
2026-07-07 14:15:52 +0530

    chore: initialize xcode project skeleton with swiftui lifecycle

---
+++ BlueScope/BlueScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueScopeApp.swift
+//  BlueScope
+//
+//  Created by Darshan Raju on 07/07/26.
+//
+
+import SwiftUI
+
+@main
+struct BlueScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
