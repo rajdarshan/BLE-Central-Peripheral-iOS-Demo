7c42b504aa7e150653da8a3b4f2b2f12b8a184c0
Darshan raju <rajdarshan1@gmail.com>
2026-07-14 12:14:52 +0530

    chore: added Test targets

---
+++ BlueScopeUITests/BlueScopeUITestsLaunchTests.swift
@@ -0,0 +1,36 @@
+//
+//  BlueScopeUITestsLaunchTests.swift
+//  BlueScopeUITests
+//
+//  Created by Darshan Raju on 14/07/26.
+//
+
+import XCTest
+
+final class BlueScopeUITestsLaunchTests: XCTestCase {
+
+    // swiftlint:disable:next static_over_final_class
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
