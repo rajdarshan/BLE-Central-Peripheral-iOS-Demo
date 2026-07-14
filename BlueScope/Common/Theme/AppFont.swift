33aa79737fb253eb08a122f1b7d8e61459514f7f
Darshan raju <rajdarshan1@gmail.com>
2026-07-14 15:31:30 +0530

    fix(theme): avoid Font.body/caption redeclaration

--- BlueScope/Common/Theme/AppFont.swift
+++ BlueScope/Common/Theme/AppFont.swift
@@ -13,9 +13,9 @@ extension Font {
     /// Font with size: 13 , weight: medium
     static let sectionHeader = Font.system(size: 13, weight: .medium)
     /// Font with size: 15 , weight: regular
-    static let body = Font.system(size: 15, weight: .regular)
+    static let bodyText = Font.system(size: 15, weight: .regular)
     /// Font with size: 12 , weight: regular
-    static let caption = Font.system(size: 12, weight: .regular)
+    static let captionText = Font.system(size: 12, weight: .regular)
     /// Font with size:14 , weight: regular
     static let monospaceValue = Font.system(size: 14, weight: .regular, design: .monospaced) // for hex/UUID display
 }
